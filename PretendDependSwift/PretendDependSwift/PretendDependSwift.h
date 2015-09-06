9812e0c4d0db37270ca5e648ddc4a1974a480775
oisdk <oisinkidney@Oisins-MacBook-Air.local>
2015-09-06 12:35:15 +0100

    Initial commit

---
+++ PretendDependSwift/PretendDependSwift/PretendDependSwift.h
@@ -0,0 +1,19 @@
+//
+//  PretendDependSwift.h
+//  PretendDependSwift
+//
+//  Created by Donnacha Oisín Kidney on 06/09/2015.
+//  Copyright © 2015 Donnacha Oisín Kidney. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PretendDependSwift.
+FOUNDATION_EXPORT double PretendDependSwiftVersionNumber;
+
+//! Project version string for PretendDependSwift.
+FOUNDATION_EXPORT const unsigned char PretendDependSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PretendDependSwift/PublicHeader.h>
+
+
